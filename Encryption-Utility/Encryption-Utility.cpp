3f4ec782ec08694f73ea9c67b888f6259f9ce932
E1Bernardo <goonypony@gmail.com>
2020-04-13 13:26:36 +0500

    Creating a Host module

--- Encryption-Utility/Encryption-Utility.cpp
+++ Encryption-Utility/Encryption-Utility.cpp
@@ -1,7 +1,26 @@
 ﻿#include <iostream>
+#include"Header_function.h"
 
-int main()
+
+
+using namespace std;
+
+int main(string file_name, string key) // можно ли перед вводом данных написать в консоль, что имеено нужно ввести?
 {
-    std::cout << "Hello World!\n";
+   cout << "Hello World!\n";
+    
+    check_file_name_and_key(file_name, key);
+    verification_encryption_or_decryption(file_name);
+
+    if (key == "ключ для шифрования")
+    {
+        encryption(file_name);
+    }
+    else
+    {
+        decryption(file_name);
+    }
+
+    output_result(file_name);
 }
 
---
+++ Encryption-Utility/Header_function.h
@@ -0,0 +1,11 @@
+
+
+void check_file_name_and_key(string& file_name, string& key);
+
+int verification_encryption_or_decryption(string file_name);
+
+void encryption(string file_name);
+
+void decryption(string file_name);
+
+int output_result(string file_name);
