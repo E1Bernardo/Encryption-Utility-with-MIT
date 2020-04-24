0c6e5291126aee06cd7ca1ed609892fa6565883a
E1Bernardo <goonypony@gmail.com>
2020-04-24 12:57:30 +0500

    Do open key for RSA encryption

--- Encryption-Utility/Encryption.cpp
+++ Encryption-Utility/Encryption.cpp
@@ -1,73 +1,43 @@
 #include <iostream>
 #include <vector>
 #include<fstream>
+#include<ctime>
+
+struct Key
+{
+	unsigned __int64 exhibitor;
+	unsigned __int64 mod; // сокращение от модуль   the abbreviation for module
+};
+
+
+
+
+int checking_number_simplicity();// Ввод и проверка числа на простоту  Enteringand checking a number for simplicity
+
+void finding_open_exponents(vector <unsigned __int64> & open_exhibitor_array, unsigned __int64 f_Euler);
+unsigned __int64 selection_open_exhibitors(vector <unsigned __int64>open_exhibitor_array);
 
 using namespace std;
 // проверку на простоту и прочие большие куски можно сделать в отдельные функции 
-
-
 void encryption(vector <unsigned __int8>& inputf)
 {
-	unsigned __int64 p, q;
-	bool check_p_2 = false , check_q_2 = false;
-	do // Проверка на натуральность первого числа  Checking for the naturalness of the first number
-	{
-		int check_p = 0;
-		cout << "Enter first prime number\n";
-		cout << "p = ";
-		cin >> p;
-		for (unsigned __int64 i = 2; i <= sqrt(p); i++)
-			if (p % i == 0) check_p++;
-
-		if (check_p == 0) check_p_2 = true;
-		else 
-		{
-			std::cin.clear();
-			std::cin.ignore(3000, '\n');
-			cout << "\nEntered number is not prime, repeat the entry:\n";
-		}
-	} while (check_p_2 == false);
-	cout << endl;
-	do //Проверка на натуральность второго числа   Checking for the naturalness of the second number
-	{
-		int check_q = 0;
-		cout << "Enter second prime number\n";
-		cout << "q = ";
-		cin >> q;
-		for (unsigned __int64 i = 2; i <= sqrt(q); i++)
-			if (q % i == 0) check_q++;
-
-		if (check_q == 0) check_q_2 = true;
-		else
-		{
-			std::cin.clear();
-			std::cin.ignore(3000, '\n');
-			cout << "\nEntered number is not prime, repeat the entry:\n";
-		}
-	} while (check_q_2 == false);
-
+	unsigned __int64 p, q; // Да простых числа   Two prime number
+	cout << "Enter two prime number:\n";
+	p = checking_number_simplicity();
+	q = checking_number_simplicity();
 
 	unsigned __int64 module = p * q;  // Модуль   Module
-	unsigned __int64 f_Euler = (p - 1) * (q - 1);  // Функция Эйлера   Func Euler
+	unsigned __int64 f_Euler = (p - 1) * (q - 1);  // Функция Эйлера   Function Euler
 	unsigned __int64 open_exhibitor;  // Открытая экспонента   Open Exhibitor
+
 	vector <unsigned __int64>open_exhibitor_array(0);
 
-	for (unsigned __int64 i = 2; i < f_Euler; i++)
-	{
-		int check = 0;
-		for (unsigned __int64 j = 1; j <= sqrt(i); j++)
-			if (i % j == 0) check++;
-		
-			if (check == 1 && i % f_Euler !=0)
-			{
-				open_exhibitor_array.insert(open_exhibitor_array.end(),i);
-			}
-	}
+	finding_open_exponents(open_exhibitor_array, f_Euler);
 
-	for (size_t i = 0; i < size(open_exhibitor_array); i++)
-	{
-		cout << open_exhibitor_array[i] << endl;
-	}
+	open_exhibitor = selection_open_exhibitors(open_exhibitor_array);
+
+	Key open_key = { open_exhibitor,module};
+
 
 	// нужно сделать что бы из вектора выбиралась рандомная открытая экспонента 
 	// сгенерировать число до границы вектора и этим числом по индексу вектора взять элемент 
@@ -75,4 +45,53 @@ void encryption(vector <unsigned __int8>& inputf)
 
 
 	cout << "yeap";
+}
+
+int checking_number_simplicity()
+{
+	bool bool_check = false;
+	int number;
+	do // Проверка на натуральность первого числа  Checking for the naturalness of the first number
+	{
+		int check_number = 0;
+		cout << "Enter prime number\n";
+		cout << "number = ";
+		cin >> number;
+		for (unsigned __int64 i = 2; i <= sqrt(number); i++) {
+			if (number % i == 0) check_number++;
+		}
+		if (check_number == 0){
+			bool_check = true;
+		}else{
+			std::cin.clear();
+			std::cin.ignore(3000, '\n');
+			cout << "\nEntered number is not prime, repeat the entry:\n";
+		}
+	} while (bool_check == false);
+
+	return number;
+}
+
+void finding_open_exponents(vector <unsigned __int64> & open_exhibitor_array, unsigned __int64 f_Euler)
+{
+	for (unsigned __int64 i = 2; i < f_Euler; i++) {
+		int check = 0;
+		for (unsigned __int64 j = 1; j <= sqrt(i); j++) {
+			if (i % j == 0) check++;
+		}
+		if (check == 1 && i % f_Euler != 0) {
+			open_exhibitor_array.insert(open_exhibitor_array.end(), i);
+		}
+	}
+}
+
+
+unsigned __int64 selection_open_exhibitors(vector <unsigned __int64>open_exhibitor_array)
+{
+	srand(time(NULL));
+	int size = sizeof(open_exhibitor_array);
+
+	int choice = 1 + rand() % sizeof(open_exhibitor_array) + 1;
+
+	return open_exhibitor_array[choice];
 }
